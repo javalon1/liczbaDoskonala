9d360379075c6f5e307ec2e534c3126a22121724
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-18 10:27:01 +0100

    liczba_doskonala_final

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,27 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int i = 1;
+        int suma = 0;
+
+
+        while (i <= x / 2) {
+            if (x % i == 0) {
+                suma += i;
+            }
+            i++;
+        }
+        if ( suma == x){
+            System.out.println(x + " jest liczba doskonala");
+        }else {
+            System.out.println(x + " nie jest liczba dsokonala");
+        }
     }
 }
+
